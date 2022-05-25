44e519b3ecd83b8f0534c6751a39255dbbea97ba
Li.Xu <li.xu@noaa.gov>
2022-05-25 00:46:41 +0000

    add cron.sh usage

--- myjobs/cron.sh
+++ myjobs/cron.sh
@@ -1,8 +1,28 @@
 #!/bin/bash -l
 
+
+#---------------------------------------------------------------------------------------------------
+# Usage
+#
+script_name=$(basename "$0")
+usage() {
+    printf "$script_name HH \n"
+    printf "  where  HH is 00 06 12 18 \n"
+}
+
+#---------------------------------------------------------------------------------------------------
+# Check command-line args
+#
+if [[ "$#" -ne 1 ]]; then
+    usage
+    exit 1
+fi
+
+
+
 cd "$( dirname "${BASH_SOURCE[0]}")"
 
-echo 'run for :' $1
+echo 'run for cyc:' $1
 
 bsub < CPCI.GDAS_$1.JSUB
 bsub < CPCI.GFS_$1.JSUB
